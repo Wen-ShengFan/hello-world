ac700260b87e0143e47c44fe263dd6199cb8ad83
sfwen <sfwen@itri.org.tw>
2022-12-29 17:07:14 -0800

    a talker-listener structure of ROS

---
+++ build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/sfwen/satelite_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/sfwen/satelite_ws/devel;/opt/ros/noetic'.split(';') if '/home/sfwen/satelite_ws/devel;/opt/ros/noetic' != '' else []
